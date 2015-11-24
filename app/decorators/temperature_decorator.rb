66effb201909efb220fb039247f66ff1aa6b5625
sert <demo.sert@gmail.com>
2015-11-24 16:56:49 +0900

    add validation

--- app/decorators/temperature_decorator.rb
+++ app/decorators/temperature_decorator.rb
@@ -2,10 +2,10 @@ class TemperatureDecorator < Draper::Decorator
   delegate_all
 
   def created_at
-    model.created_at.strftime('%Y年%m月%d日 %H時%M分%S秒')
+    model.created_at.strftime('%Y年%m月%d日 %H時%M分')
   end
 
   def updated_at
-    model.updated_at.strftime('%Y年%m月%d日 %H時%M分%S秒')
+    model.updated_at.strftime('%Y年%m月%d日 %H時%M分')
   end
 end
--- app/models/temperature.rb
+++ app/models/temperature.rb
@@ -1,2 +1,9 @@
 class Temperature < ActiveRecord::Base
+  before_validation :same_data_check
+
+  private
+
+  def same_data_check
+    Temperature.where(created_at: 1.minutes.ago..Time.now).blank?
+  end
 end
