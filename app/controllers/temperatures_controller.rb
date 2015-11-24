1dd2bab5a9639b05d4b140d8be11978f8602659a
sert <demo.sert@gmail.com>
2015-11-24 15:48:04 +0900

    implements create method

--- app/controllers/temperatures_controller.rb
+++ app/controllers/temperatures_controller.rb
@@ -1,10 +1,19 @@
 class TemperaturesController < ApplicationController
   protect_from_forgery except: [:create]
 
-  def new
+  def create
+    temp = Temperature.new(temp_params)
+
+    if temp.save
+      render status: 200, json: { 'message': 'OK' }
+    else
+      render status: 422, json: { 'message': 'failure' }
+    end
   end
 
-  def create
-    render status: 200, json: { 'message': 'OK' }
+  private
+
+  def temp_params
+    params.require(:temperature).permit(:temp)
   end
 end
